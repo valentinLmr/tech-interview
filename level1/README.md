9930d399b010ab8d7783f49487671edeef677937
Julien Polo <julien.polo@koober.com>
2018-06-05 20:00:27 +0200

    Create draft for first level

---
+++ level1/README.md
@@ -0,0 +1,3 @@
+# Level 1
+
+Write code that writes to stdout from `input.json`
---
+++ level1/index.js
@@ -0,0 +1,9 @@
+
+function main() {
+  // Write answer to standard output
+  process.stdout.write('{}');
+}
+
+if (require.main === module) {
+  main();
+}
