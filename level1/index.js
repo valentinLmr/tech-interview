5c24b5b3cf2e127d084125497795aaf7c61886ff
valentin lmr <valentinlmr28@gmail.com>
2020-02-12 22:25:17 +0100

    Préparation du Parsing JSON

--- level1/index.js
+++ level1/index.js
@@ -1,8 +1,34 @@
+// function main() {
 
-function main() {
-  
-}
+// }
 
-if (require.main === module) {
-  main();
-}
+// if (require.main === module) {
+//   main();
+// }
+
+fetch('./input.json')
+.then(function(resp){
+  return resp.json();
+})
+.then(function(data){
+  let outputArticleName = " "
+  const articles = data.articles
+  for(i = 0; i < articles.length; i++){
+  outputArticleName += '<li>' + articles[i].name + ' ' + articles[i].price + '</li>'
+  }
+  // document.getElementById('mydata').insertAdjacentHTML('beforeend',outputArticleName)
+
+console.log(data.carts[0].items)
+  let outputCartId = ""
+  const carts = data.carts
+  for(i = 0; i < carts.length; i++){
+  const items = data.carts[i].items
+  console.log(items)
+  // outputArticleName += '<li>' + carts[i].name + ' ' + carts[i].price + '</li>'
+  }
+  // document.getElementById('mydata').insertAdjacentHTML('beforeend',output)
+
+
+
+})
+
