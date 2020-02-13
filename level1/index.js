02f89b7aed27d4fae7d874ec9d115b8aa86e9da7
valentin lmr <valentinlmr28@gmail.com>
2020-02-13 20:17:20 +0100

    select and add in cart

--- level1/index.js
+++ level1/index.js
@@ -29,18 +29,96 @@ fetch('./input.json')
   for(i = 0; i < carts.length; i++){
     outputCartId += '<div class=cart data-id='+ carts[i].id + '><h3> cart ID:' + carts[i].id + '</h3>'
     for(index = 0; index < carts[i].items.length; index++){
-      outputCartId += '<div class=item> <h4 class=article_id data-id='+ carts[i].items[index].article_id +'> Article ID:' + carts[i].items[index].article_id +'</h4><p class=article-qty> Quantité: '+ carts[i].items[index].quantity + '</p></div>';
+      outputCartId += '<div class=item data-id='+ carts[i].items[index].article_id +'> <h4 class=article_id> Article ID:' + carts[i].items[index].article_id +'</h4><p class=article-qty>'+ carts[i].items[index].quantity + '</p></div>';
     }
     outputCartId += '</div>'
   }
  document.getElementById('carts').insertAdjacentHTML('beforeend',outputCartId)
 
+
+  const addArticle = (event) => {
+    let articleId  = event.dataset.id
+    const carts    = document.querySelectorAll(".cart")
+    carts.forEach((cart) => {
+
+      cart.addEventListener("click", (event) =>{
+
+        const items = event.currentTarget.querySelectorAll(".item")
+        items.forEach((item) => {
+
+          if(item.dataset.id === articleId){
+            let itemQty = item.querySelector(".article-qty")
+            let qtyInt  = parseInt(itemQty.innerText, 10)
+            itemQty.innerText = qtyInt += 1
+          }
+
+          // }else {
+
+          //   const addArticleToCart = '<div class="item" data-id='+ articleId+'> <h4 class="article_id"> Article ID:'+ articleId+'</h4><p class="article-qty">1</p></div>'
+          //   event.currentTarget.insertAdjacentHTML('beforeend', addArticleToCart)
+          // }
+        })
+
+        articleId = ""
+
+      })
+    })
+  }
   const allArticles = document.querySelectorAll(".article")
-  console.log(allArticles)
+  allArticles.forEach((article) => {
+    article.addEventListener("click", (event) =>{
+      addArticle(event.currentTarget);
+
+    });
+  });
 })
 
 
 
+
+
+// carts = { carts: [
+//   {
+//     id:    'Edelweiss',
+//     total: 'White',
+//   },
+//   {
+//     id:    'Guinness',
+//     total: 'Stout',
+//   },
+//   {
+//     id:    'Edelweiss',
+//     total: 'White',
+//   }
+// ]}
+
+// File.open(filepath, 'wb') do |file|
+//   file.write(JSON.generate(beers))
+// end
+
+
+
+ // const toJson = (article) =>{
+ //    const cartsToJson     = document.querySelectorAll(".cart")
+ //    cartsToJson.forEach((cart)  => {
+ //      const itemsToJson   =  cart.querySelectorAll(".item")
+ //      itemsToJson.forEach((item)=> {
+ //        if(item.id == article.id){
+
+ //          const jsonId = item.id
+ //          const priceJson = article.querySelector(".price")
+ //          const qty       = item.querySelector(".article-qty")
+ //          const qtyJson   = parseInt(qty.innerText,10)
+ //          const totalJson = qtyJson * priceJson.dataset.price
+ //          const dataJson  = "{ 'id':" + jsonId + ',' + 'total:' + totalJson + " }"
+ //          console.log(dataJson)
+
+
+ //        }
+ //      })
+
+ //    })
+ //  }
 
 
 
