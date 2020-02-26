34d8a3c0a78de656fd33cdcdc6001982d87b0d1e
valentin lmr <valentinlmr28@gmail.com>
2020-02-26 09:01:39 +0100

    level 1 done

--- level1/index.js
+++ level1/index.js
@@ -1,137 +1,237 @@
-
-// import { json } from "./plugins/json" ;
-// import { selectArticle } from "./plugins/addArticleToCart"
-// import { displayArticle } from "./plugins/displayArticleHtml"
-// import { displayCart } from "./plugins/displayCartHtml"
-
-
-
-fetch('./input.json')
-.then(function(resp){
-  return resp.json();
-})
-.then(function(data){
-
-  //display ARTICLES in my HTML
-  // displayArticle();
-// const displayArticle = () => {
-  let outputArticleName  = " "
-  let outputArticlePrice = " "
-  const articles         = data.articles
-  for(i = 0; i < articles.length; i++){
-    outputArticleName += '<div class="article"  data-id="'+ articles[i].id +'"><h3> name: ' + articles[i].name + '</h3> <p class=price data-price='+ articles[i].price +'> Price: ' + articles[i].price + ' €</p></div>'
+class Article {
+  constructor(){
   }
-  document.getElementById('articles').insertAdjacentHTML('beforeend',outputArticleName)
-// }
 
-
-
-
-  // display CART in my HTML
-  // displayCart();
-  let outputCartId = " "
-  let outputItem   = " "
-  const carts      = data.carts
-
-  for(i = 0; i < carts.length; i++){
-    outputCartId += '<div class=cart data-id='+ carts[i].id + '><h3> cart ID:' + carts[i].id + '</h3>'
-    for(index = 0; index < carts[i].items.length; index++){
-      outputCartId += '<div class=item data-id='+ carts[i].items[index].article_id +'> <h4 class=article_id> Article ID:' + carts[i].items[index].article_id +'</h4><p class=article-qty>'+ carts[i].items[index].quantity + '</p></div>';
-    }
-    outputCartId += '</div>'
-  }
- document.getElementById('carts').insertAdjacentHTML('beforeend',outputCartId)
-
-
-  // Select and Add Article To cart
-  // selectArticle();
-  const addArticle = (event) => {
-  let articleId  = event.dataset.id
-  const carts    = document.querySelectorAll(".cart")
-  carts.forEach((cart) => {
-    cart.addEventListener("click", (event) =>{
-      const items = event.currentTarget.querySelectorAll(".item")
-      items.forEach((item) => {
-        if(item.dataset.id === articleId){
-          let itemQty       = item.querySelector(".article-qty")
-          let qtyInt        = parseInt(itemQty.innerText, 10)
-          itemQty.innerText = qtyInt += 1
-        } else {
-          // let arrWithdstrings = []
-          let arrWithdstrings = '<div class="item" data-id='+ articleId+'> <h4 class="article_id"> Article ID:'+ articleId+'</h4><p class="article-qty">1</p></div>'
-          event.currentTarget.insertAdjacentHTML('beforeend', arrWithdstrings)
-        }
-      })
-      articleId = ""
+  async getData() {
+    await fetch('./input.json')
+    .then((response) => {
+       return response.json();
     })
-  })
-};
-
-// const selectArticle = () => {
-  const allArticles = document.querySelectorAll(".article")
-  allArticles.forEach((article) => {
-    article.addEventListener("click", (event) =>{
-      addArticle(event.currentTarget);
-    });
-  });
-// }
-  // sent data to OutputJson
-  // json();
-
-  const json = () => {
-  const cartsToJson   = document.querySelectorAll(".cart")
-  let totalJson       = 0
-  let  dataJson       = "{carts :}"
-  cartsToJson.forEach((cart)  => {
-    dataJson          += "{id : cart.dataset.id"
-    const allItems    = cart.querySelectorAll(".item")
-    allItems.forEach((item) => {
-      let arrayItemId = [] ;
-    const allArticles = document.querySelectorAll(".article")
-    allArticles.forEach((article) => {
-        if (item.dataset.id === article.dataset.id){
-          const priceJson   = article.querySelector(".price")
-          const qty         = item.querySelector(".article-qty")
-          const qtyJson     = parseInt(qty.innerText,10)
-          totalJson         += qtyJson * priceJson.dataset.price
-        }
-      });
-    });
-    dataJson  += ",total:totalJson},"
-    totalJson = 0
-  });
-  dataJson =  dataJson.split("")
-  dataJson.pop()
-  dataJson = dataJson.join("")
-  dataJson += "]}"
-
-  console.log(dataJson)
-  let data = dataJson
-  const f = "output.json";
-  writeTextFile(f, dataJson)
-
-  function writeTextFile(afilename, output)
-  {
-    var txtFile = new File(afilename, f);
-    txtFile.writeln(output);
-    txtFile.close();
+    .then((data) => {
+     this.result = data.articles
+     // console.log(this.result)
+    })
+    .catch(function (error) {
+        alert(error);
+    })
   }
-
-
-
-
-  // const fs = require('fs');
-  // fs.writeFileSync('./output.json', data => {
-  // if (data) throw data;
-  //   console.log('The file has been saved!');
 }
-json()
+
+class Cart {
+  constructor(){
+  }
+  async getData() {
+    await fetch('./input.json')
+    .then((response) => {
+       return response.json();
+    })
+    .then((data) => {
+     this.result = data.carts
+     // console.log(this.result)
+    })
+    .catch(function (error) {
+        alert(error);
+    })
+  }
+}
+
+class Object {
+  constructor(carts){
+    this.carts = carts = []
+  }
+}
 
 
-})
+const element = {
+  searchCartsDiv: document.querySelector(".carts"),
+  searchArticlesDiv: document.querySelector(".articles")
+}
+
+// global state of the app
+const state = {};
+
+
+const controlObject = () => {
+state.object = new Object("carts")
+console.log(state.object)
+}
+
+const controlArticle = async() => {
+  state.articles = new Article();
+  await state.articles.getData();
+  displayArticles(state.articles.result);
+}
+
+const controlCart = async() => {
+  state.carts = new Cart();
+  await state.carts.getData();
+  displayCarts(state.carts.result);
+}
+
+const clearCartsView = () => {
+  element.searchCartsDiv.innerHTML = '' ;
+}
+
+
+const findArticle = (item, type) => {
+  const artItem = state.articles.result.find(element => element.id === item.article_id)
+  return getHtmlForItem(artItem, item)
+}
+
+const findCartInData = cartId => {
+  object = {
+    carts: []
+  }
+  let arrayPrice = [];
+  let cartToCo, article, totalPrice, cartObject;
+  cartToCo = state.carts.result.find(el => el.id === parseInt(cartId, 10))
+  cartToCo.items.forEach(item => {
+    article = state.articles.result.find(element => element.id === item.article_id)
+
+    arrayPrice.push(article.price * item.quantity)
+  })
+  total = arrayPrice.reduce((arr, curr) => arr + curr )
+  id = parseInt(cartId, 10),
+
+
+   outputDataJson(object, total, parseInt(cartId, 10));
+}
+
+const outputDataJson = (object, total, id) => {
+
+  state.object.carts.push(
+  { id: id,
+    total: total
+  })
+
+  let json = JSON.stringify(state.object)
+  fs.writeFile('ouput.json', json, function(err) {
+    if (err) throw err;
+    console.log('complete');
+    });
+  console.log(state.object)
+}
+
+const getHtmlForItem = (article, item) =>`
+<div class="item">
+  <div class="item__infos">
+    <div class="article__name" data-id="${article.id}">
+      ${article.name}
+    </div>
+    <div class="article__qty" data-qty="${item.quantity}">
+        ${item.quantity}
+    </div>
+  </div>
+   <div class="item__delete">
+        <i class="far fa-times-circle"></i>
+    </div>
+</div>
+`;
+
+const getHtmlArticles = article =>
+`
+    <div class="article" data-id="${article.id}">
+      <p> ${article.name}</p>
+      <p>${article.price / 100} €</p>
+    </div>
+`;
+
+const getHtmlCarts = cart => `
+
+<div class="cart" data-id="${cart.id}">
+  <h3>Cart n°${cart.id}</h3>
+  <div class="cart_header">
+    <p>Name</p>
+    <p>Quantity</p>
+  </div>
+  <div class="items">
+    ${cart.items.map(el => findArticle(el, 'html')).join('')}
+  </div>
+  <button type="button" class="btn btn-danger">CheckOut Cart</button>
+</div>
+`;
 
 
 
+const displayArticles = articles => {
+  articles.forEach(article => {
+    if(article){
+      element.searchArticlesDiv.insertAdjacentHTML('beforeend', getHtmlArticles(article))
+    }
+  })
+}
+
+const displayCarts = carts => {
+  clearCartsView();
+  carts.forEach(cart => {
+    if(cart.items.length > 0){
+      element.searchCartsDiv.insertAdjacentHTML('beforeend', getHtmlCarts(cart))
+    }
+  })
+}
+
+
+const selectArticleForCard = () => {
+  let article, cart, articleDiv;
+
+  element.searchArticlesDiv.addEventListener("click", e => {
+    articleDiv = e.target.closest(".article");
+    if(articleDiv){
+      articleDiv.classList.add('active')
+
+      element.searchCartsDiv.addEventListener("click", e => {
+        const cartDiv = e.target.closest(".cart");
+        cart = cartDiv.dataset.id
+        addArticle(articleDiv, cart)
+      articleDiv = '';
+      });
+    }
+  });
+}
+
+const addArticle = (articleDiv, cart) => {
+  let articleId, stateCart, element;
+
+  if(articleDiv) {articleDiv.classList.remove('active')
+
+    articleId = parseInt(articleDiv.dataset.id, 10)
+    stateCart = state.carts.result.find(e => e.id === parseInt(cart,10))
+    if(stateCart) {
+      element = stateCart.items.find(e => e.article_id === articleId)
+      if(element){
+        element.quantity += 1
+      }else{ stateCart.items.push(
+          {
+            article_id: articleId,
+            quantity: 1
+          }
+        );
+      }
+    }
+  }
+  displayCarts(state.carts.result);
+}
+
+
+const SelectCartTocheckOut = () => {
+  let cartToCo;
+  element.searchCartsDiv.addEventListener('click', e => {
+    if(e){
+      cartToCo = e.target.closest('.cart').dataset.id;
+      findCartInData(cartToCo);
+    }
+  })
+}
+
+const init = () => {
+  controlArticle();
+  controlCart();
+  selectArticleForCard();
+  SelectCartTocheckOut();
+  controlObject();
+}
+
+init();
 
 
 
